367fe61ae20b304c406652dcfa5e8ea439212d22
Alexandros Tsingilis <6560751+alextsigilis@users.noreply.github.com>
2024-06-21 04:05:00 +0300

    First commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Enable Greek Commands
+vim.o.langmap = "ΑA,ΒB,ΓC,ΔD,ΕE,ΖZ,ΗH,ΘQ,ΙI,ΚK,ΛL,ΜM,ΝN,ΞJ,ΟO,ΠP,ΡR,ΣS,ΤT,ΥU,ΦF,ΧX,ΨY,ΩW,αa,βb,γc,δd,εe,ζz,ηh,θq,ιi,κk,λl,μm,νn,ξj,οo,πp,ρr,σs,τt,υu,φf,χx,ψy,ωw"
+
+-- Enable realtive line numbers
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
